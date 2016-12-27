27bdf6361004a06e888dee403c98b71b43564c22
farhanmohdtahir <farhanmohdtahir@gmail.com>
2016-12-27 09:06:58 +0800

    PEAdapterFinder code

---
+++ Input.h
@@ -0,0 +1,15 @@
+#ifndef INPUT_H
+#define INPUT_H
+#include "string"
+
+using namespace std;
+class Input{
+public:
+    int checkNucleotide(string , string& );
+    int complementInput(string&);
+    string reform(string, bool&);
+};
+
+
+#endif /* INPUT_H */
+
