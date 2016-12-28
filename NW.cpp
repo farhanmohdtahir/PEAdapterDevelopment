c6ddf2d30a28f44997e2d12f6005a4b056d9ff02
farhanmohdtahir <farhanmohdtahir@gmail.com>
2016-12-28 11:24:18 +0800

    overcome segmentation fault

--- NW.cpp
+++ NW.cpp
@@ -164,7 +164,7 @@ int NW::nw_align(
         }
         j = rowmax;
 
-        while( i > 0  || j > 0 )
+        while( i > 0  && j > 0 )
         {
             if (nuc[j-1]!='N'||nuc2[i-1]!='N'){    
                         if (nuc[j-1]==nuc2[i-1]){
